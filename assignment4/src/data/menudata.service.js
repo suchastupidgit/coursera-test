954f99173859a5f79cbaf59465eba007b5b50f8e
suchastupidgit <stevensykes.2010@yahoo.com>
2016-12-19 11:29:12 -0500

    better service

--- assignment4/src/data/menudata.service.js
+++ assignment4/src/data/menudata.service.js
@@ -9,48 +9,21 @@ MenuDataService.$inject = ['$http', 'ApiBasePath']
 function MenuDataService($http, ApiBasePath) {
   var service = this;
 
-  // List of shopping catgories and items
-  service.categories = [];
-  service.items = [];
-
-  service.getAllCategories = function () {
-    console.log("getAllCategories()");
-    var response = $http({
-      method: "GET",
-      url: (ApiBasePath + "/categories.json")
-    });
-
-    response.then(function(response) {
-      var count = response.data.length;
-      console.log("categories count = " + count)
-      service.categories.length = 0;
-      for(var i = 0; i < count; i++) {
-        service.categories.push(response.data[i]);
-      }
-    });
-
-    return service.categories;
+  // Elegant simple version
+  service.getAllCategories = function() {
+    return $http.get(ApiBasePath + "/categories.json")
+      .then(function(response) {
+        return response.data;
+      });
   };
 
-  service.getItemsForCategory = function (categoryShortName) {
-    console.log("getItemsForCategory(" + categoryShortName + ")");
-    var response = $http({
-      method: "GET",
-      url: (ApiBasePath + "/menu_items.json?category=" + categoryShortName)
-    });
-
-    response.then(function(response) {
-      var count = response.data.menu_items.length;
-      console.log("items count = " + count)
-      service.items.length = 0;
-      for(var i = 0; i < count; i++) {
-        service.items.push(response.data.menu_items[i]);
-      }
-    });
-
-    return service.items;
+  // Elegant simple version
+  service.getItemsForCategory = function(categoryShortName) {
+    return $http.get(ApiBasePath + "/menu_items.json?category=" + categoryShortName)
+      .then(function(response) {
+        return response.data.menu_items;
+      });
   };
-
 
 }
 
